669101d5986e570c4643868dbcc0d40ebf649010
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-26 22:37:01 +0530

    axios installation and familiarization

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import NavBar from "./Components/NavBar/NavBar";
 import './App.css';
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [state,setState]=useState([]);
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost /> */}
+
+
+      {/* API response has our necessary data in response.data */}
+      <button onClick={()=>
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response)=>{console.log(response.data);
+          setState(response.data);
+        })
+    }>Click me</button>
+
+    {/* index is second parameter  and is not required */}
+    {/* map has a return;return can have ony one div */}
+    {state.map((obj,index)=>{
+      return (
+        <div>
+          <h1>{index+1}</h1>
+          <h2>{obj.title}</h2>
+          <p>{obj.body}</p>
+        </div>
+      )
+    })}
     </div>
   );
 }
